7717e576c534987f287a361f64d6d5a5ae11e608
Patrick Nicholson <patrick.nicholson@outlook.com>
2023-03-11 17:47:37 -0500

    don't format original notebook

--- notebooks/convert.sh
+++ notebooks/convert.sh
@@ -3,11 +3,16 @@
 source /opt/homebrew/anaconda3/bin/activate base
 
 for arg; do
-  black --preview --line-length 65 "$arg"
-  jupyter nbconvert "$arg" \
+  cd "${arg:h}" || exit
+  cp "${arg:t}" nb.ipynb
+  black --preview --line-length 65 nb.ipynb
+  jupyter nbconvert nb.ipynb \
     --to markdown \
+    --NbConvertApp.output_files_dir "${arg:t:r}_files" \
     --TagRemovePreprocessor.enabled=True \
     --TagRemovePreprocessor.remove_cell_tags remove_cell \
     --TagRemovePreprocessor.remove_all_outputs_tags remove_output \
     --TagRemovePreprocessor.remove_input_tags remove_input
+  mv nb.md "${arg:t:r}.md"
+  rm nb.ipynb
 done
